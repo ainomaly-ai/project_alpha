75e410dc369821e5ca0a8be67263bbd3e6efc0cb
ainomaly101 <ainomaly@proton.me>
2025-05-11 20:06:27 +0200

    changes for dealing with events

--- alpha/telegram/token_safety.py
+++ alpha/telegram/token_safety.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from typing import Optional
 from telethon import TelegramClient, events
 from dotenv import load_dotenv
+import asyncio
+import time
 
 load_dotenv()
 
@@ -20,7 +22,7 @@ group_chat_ids = ["BananaGun_bot", "fluxbeam_bot", "solana_trojanbot", "TrenchSc
 class SafetyCheck(BaseModel):
     ca : Optional [str] = None
 
-    async def main(self):
+    async def safety(self):
         """
         Connects to Telegram, send and starts listening for new messages, and keeps the connection alive.
         """
@@ -53,3 +55,43 @@ class SafetyCheck(BaseModel):
             
             # Keep the client running until manually stopped
             await client.run_until_disconnected()
+
+    async def get_details(self):
+        """
+        Connects to Telegram, send and starts listening for new messages, and keeps the connection alive.
+        """
+        queue = asyncio.Queue()
+        async with TelegramClient(session_path, api_id, api_hash) as client:
+        
+            await client.connect()
+
+            # for group_id in group_chat_ids:
+            await client.send_message("BananaGun_bot", self.ca)
+            # await client.send_message(f"th_sol_scanner_bot", "/thp {self.ca}") 
+            
+            print(f"Message sent to bot")
+
+            # time.sleep(2)
+
+
+            print("Listening for new messages...")
+
+            @client.on(events.NewMessage(chats="BananaGun_bot"))
+            async def my_new_message_handler(event):
+                """
+                This function is called whenever a new message is received.
+                """
+
+                message_text = event.message.text
+                await queue.put(message_text)
+                await client.disconnect()
+
+            await client.run_until_disconnected() 
+        return  await queue.get()
+            
+
+            
+            # # Keep the client running until manually stopped
+            # await client.run_until_disconnected()
+
+
